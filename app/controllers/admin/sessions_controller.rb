4fa97480b5541368a639426d84c60d73ac59185c
uma <umabaligranado@gmail.com>
2023-06-08 14:22:51 -0300

    ..devise

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,14 @@
+lass Admin::SessionsController < Devise::SessionsController
+  before_action :check_credentials, only: [:create, :delete, :update]
+
+  private
+
+  def check_credentials
+    email = params[:admin][:email]
+    password = params[:admin][:password]
+
+    unless email == Admin.new.email && password == Admin.new.password
+      redirect_to new_admin_session_path, alert: 'Credenciales inválidas'
+    end
+  end
+end
