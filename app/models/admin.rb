2d80ab59734f81e40f46c072d8fec2ac730653a8
uma <umabaligranado@gmail.com>
2023-06-08 14:31:59 -0300

    config/admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,16 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, authentication_keys: [:email], :registerable,
          :recoverable, :rememberable, :validatable
+        
+    def email
+      restaurant@gmail.com
+    end
+        
+    def password
+     123456
+    end
+  end
+        
 end
